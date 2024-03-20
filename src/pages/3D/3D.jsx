ede622479e9a36f88178f81de0fdbe57e2be5436
Claus Hesse <claus.h@oppizi.com>
2024-03-19 22:47:31 -0300

    add routing

---
+++ src/pages/3D/3D.jsx
@@ -0,0 +1,9 @@
+import { Layout } from '../../components/Layout'
+
+export const Model = () => {
+  return (
+    <Layout>
+      <div>3D Model</div>
+    </Layout>
+  )
+}
---
+++ src/pages/Funzionalita/Funzionalita.jsx
@@ -0,0 +1,9 @@
+import { Layout } from '../../components/Layout'
+
+export const Funzionalita = () => {
+  return (
+    <Layout>
+      <div>Funzionalita</div>
+    </Layout>
+  )
+}
