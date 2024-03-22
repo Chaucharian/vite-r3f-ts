4c715180d4687876fb37eb696f60b5ee687c8a5c
Agustin Salinas <agustinsalinas@Agustins-MacBook-Pro.local>
2024-03-21 23:08:11 -0300

    fix - conditional ci base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const isProd = process.env.NODE_ENV === 'CI'
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/vite-r3f-ts/',
+  base: isProd ? '/vite-r3f-ts/' : '',
   plugins: [react()],
   assetsInclude: ['**/*.gltf','**/*.glb'],
 })
