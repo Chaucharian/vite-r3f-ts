4f343794e995576f5d77fb24519524437482617e
Agustin Salinas <agustinsalinas@Agustins-MacBook-Pro.local>
2024-03-21 22:23:11 -0300

    fix - compile fixed and models removed

--- src/components/LegalAdvice/LegalAdvice.tsx
+++ src/components/LegalAdvice/LegalAdvice.tsx
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core'
-import { styles } from './styles.ts'
-const useStyles = makeStyles(() => styles)
+import { styles } from './styles'
+const useStyles = makeStyles(():any => styles)
 
 export const LegalAdvice = () => {
-  const classes = useStyles()
+  const classes:any = useStyles()
   return (
     <div className={classes.adviceContainer}>
       <span>
