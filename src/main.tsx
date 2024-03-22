a8c9ce7a7a54ae0859f4995b5648b2a2b14afdea
Claus Hesse <claus.h@oppizi.com>
2024-03-22 11:04:23 -0300

    add fullscreen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Dashboard, Home, Hyper, Funzionalita, Uso, Model, QrCode, Bye } from './pages'
 import './styles/main.css'
 
 const App = () => {
+  const toggleFullScreen = () => {
+    if (!window.document.fullscreenElement) {
+      window.document.body.requestFullscreen()
+    }
+  }
+
+  const handleUserKeyPress = (event: any) => {
+    const { key } = event
+    if (key === 'f') {
+      toggleFullScreen()
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleUserKeyPress)
+    return () => {
+      window.removeEventListener('keydown', handleUserKeyPress)
+    }
+  }, [handleUserKeyPress])
+
   return (
     <BrowserRouter basename='vite-r3f-ts'>
       <Routes>
